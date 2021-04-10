f02c956968959c983a0c22e8391fc84ff7a5807d
eddieVim <1548033119@qq.com>
2021-04-10 14:30:27 +0800

    add static and dynamic proxy pattern.

---
+++ src/main/java/pers/eddievim/dp/structural/proxy/DynamicProxyPatternDemo.java
@@ -0,0 +1,11 @@
+package pers.eddievim.dp.structural.proxy;
+
+/**
+ * @author eddie
+ * @create 2021/4/10 14:30
+ */
+public class DynamicProxyPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pers/eddievim/dp/structural/proxy/StaticProxyPatternDemo.java
@@ -0,0 +1,12 @@
+package pers.eddievim.dp.structural.proxy;
+
+/**
+ * @author eddie
+ * @create 2021/4/10 14:28
+ */
+public class StaticProxyPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
