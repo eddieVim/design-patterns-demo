e42ee1a950eacdddbd077192517a0c3888f43c47
eddieVim <1548033119@qq.com>
2021-04-10 16:08:41 +0800

    add adapter pattern.

---
+++ src/main/java/pers/eddievim/dp/structural/adapter/ClazzAdapterPatternDemo.java
@@ -0,0 +1,11 @@
+package pers.eddievim.dp.structural.adapter;
+
+/**
+ * @author eddie
+ * @create 2021/4/10 15:32
+ */
+public class ClazzAdapterPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pers/eddievim/dp/structural/adapter/ObjectAdapterPatternDemo.java
@@ -0,0 +1,11 @@
+package pers.eddievim.dp.structural.adapter;
+
+/**
+ * @author eddie
+ * @create 2021/4/10 15:41
+ */
+public class ObjectAdapterPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
